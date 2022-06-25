67e3d0f3fa19f5c3d9378431e78db2a3cdea3e92
EldersJavas <55266266+EldersJavas@users.noreply.github.com>
2022-06-25 07:48:14 +0000

    feat✨:  update build-win.bat

--- build-win.bat
+++ build-win.bat
@@ -1,6 +1,6 @@
 del /F /S /Q govcl-template.exe
 del /F /S /Q .\win64\govcl-template.exe
-go build -buildmode=exe
+go build -buildmode=exe -i -ldflags="-H windowsgui"
 powershell Copy-Item govcl-template.exe ./win64/
 cd win64
 govcl-template.exe
